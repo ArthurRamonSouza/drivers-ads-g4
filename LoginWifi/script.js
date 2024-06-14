7f6eb90b170d8bcca28177c4ca4ed1497b73f19f
ozairjunior <ozairjunior23@outlook.com>
2024-06-14 00:49:45 -0300

    firebase
    tentativa de criar uma comunicação com o firebase

--- LoginWifi/script.js
+++ LoginWifi/script.js
@@ -1,6 +1,29 @@
-document.getElementById('registration-form').addEventListener('submit', function(event) {
+/*document.getElementById('registration-form').addEventListener('submit', function(event) {
     
     event.preventDefault();
     window.location.href = 'https://driveadsgo.com/';
+  });*/
+  
+
+
+  document.getElementById('registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    const data = {
+      gender: formData.get('gender'),
+      age: formData.get('age'),
+      email: formData.get('email')
+    };
+  
+    firebase.database().ref('registrations').push(data)
+      .then(() => {
+        
+        window.location.href = 'https://driveadsgo.com/';
+      })
+      .catch((error) => {
+        console.error('Erro ao salvar os dados:', error);
+        alert('Erro ao salvar os dados. Tente novamente.');
+      });
   });
   
