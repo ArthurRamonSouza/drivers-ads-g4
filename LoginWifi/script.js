2ed756c3045899c09209d20f0da5f0265e71b5c0
ozairjunior <ozairjunior23@outlook.com>
2024-06-14 00:27:17 -0300

    TelaLoginWifi
    tela para usuario logar no wifi

---
+++ LoginWifi/script.js
@@ -0,0 +1,6 @@
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+    window.location.href = 'https://driveadsgo.com/';
+  });
+  
