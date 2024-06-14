f25d8ad35b17d8d55b3acfca53923fe8eb58ee7d
Hiago Kalil <125253798+hiagoka@users.noreply.github.com>
2024-06-14 14:28:28 -0300

    Create page.tsx

---
+++ src/app/components/Botao/page.tsx
@@ -0,0 +1,13 @@
+import './Botao.css'
+
+function Botao() {
+    return (
+        <div>
+            <button type='submit' className="Botao">
+                Entrar
+            </button>
+        </div>
+    );
+}
+
+export default Botao
